1ca9fd664a1c986f8d0405d2bfe95f77a921f9b4
YosionBestyMarpaung <147596895+YosionBestyMarpaung@users.noreply.github.com>
2023-11-01 18:23:49 +0700

    Update alpro.py

--- alpro.py
+++ alpro.py
@@ -1,4 +1,4 @@
-"""
+ """
     Kelas: 2023E
     Prodi: D4 Manajemen Informatika
     Kelompok: 10
@@ -8,33 +8,34 @@
     2. Aditya Revangga Dwi Putra - 23091397167
     3. Yosion Besty Marpaung - 23091397168
 """
-
 #Harga Awal
 total_harga = 0
 
 #Menampilkan Menu
 print(
     """
-    Topping Pizza :
+    Pilihan Topping :
     1. Frankfurter BBQ
     2. Meat Monsta
     3. Super Supreme
     4. Super Supreme Chicken
-    """)
-
+    """
+)
 #Memilih Topping Pizza yang ingin dimakan
 topping_pizza = int(input("Pilih Topping Pizza: "))
 if topping_pizza == 1:
     topping_pizza = "Frankfurter BBQ"
+    total_harga += 43_637
 elif topping_pizza == 2:
     topping_pizza = "Meat Monsta"
+    total_harga += 43_637
 elif topping_pizza == 3:
     topping_pizza = "Super Supreme"
+    total_harga += 43_637
 elif topping_pizza == 4:
     topping_pizza = "Super Supreme Chicken"
-else:
-    print("Pilihan ukuran tidak valid.")
-    
+    total_harga += 43_637
+=   
 #Menampilkan Pilihan Crust dan Pilihan Ukuran
 print(
     """
